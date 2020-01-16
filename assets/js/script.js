c9cfa41fe9af049fab3a17740fc37033100978f3
DL <deleucecile@outlook.fr>
2020-01-16 16:53:43 +0100

    ajout du time

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,14 +2,23 @@
 function cligno (){
    $('.bloc1').fadeOut(800).fadeIn(800);
 }
+//je définis ma variable x
+var x;
 $(document).ready(function(){
-    setInterval('cligno()',2200);
+//Similaire à setTimeout, elle déclenche répétitivement la même action à intervalles réguliers.
+    x = setInterval('cligno()',2200);
+//setTimeout définit une action à exécuter et un délai avant son exécution
+    setTimeout('myStopFunction()', 5000);
 });
+function myStopFunction() {
+//stop la fonction Interval
+  clearInterval(x);
+}
 //FIn du 1er boutons qui clignote
 
-//
+//Au clique changement de couleur
 // $(function(){
-// $('div').click(function(){
-// $(this).toggleClass('background');
+// $('.bloc').click(function(){
+// $(this).toggleClass(background);
 // });
 // });
